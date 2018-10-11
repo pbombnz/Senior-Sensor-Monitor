a47996626a7c9c5d440a2c0d1b904937f365450c
Prashant B <prashant_bhikhu@hotmail.com>
2018-10-12 01:18:38 +1300

    Added Heatmap and counter functionality for motion detection

--- src/pages/senior-status/senior-status.ts
+++ src/pages/senior-status/senior-status.ts
@@ -2,22 +2,50 @@ import { Component, ChangeDetectorRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MqttClientProvider } from '../../providers/mqtt-client/mqtt-client';
 
+declare const h337: any;
+
 @Component({
   selector: 'page-senior-status',
   templateUrl: 'senior-status.html'
 })
-
 export class SeniorStatusPage {
+  objectKeys = Object.keys;
+
   lastRefreshed: number;
 
   private timer: number;
+  private heatmap;
 
   constructor(public navCtrl: NavController, private changeDetectorRef: ChangeDetectorRef, public mqtt: MqttClientProvider) {
     this.changeDetectorRef.detach();
     this.timer = setInterval(_ => {
       this.lastRefreshed = Date.now();
+
+      //let d = Object.assign(this.generateHousePos(), { value: this.getRandomInt(1,10)});
+      //this.heatmap.addData(d);
+      if(this.heatmap && this.mqtt.motionActivity_heatmapData) {
+        this.heatmap.setData({
+          data: this.mqtt.motionActivity_heatmapData
+        });
+      }
+
       this.changeDetectorRef.detectChanges();
     }, 1000);
+  }
+
+  ngAfterViewInit() {
+    this.heatmap = h337.create({
+      container: window.document.querySelector('#heatmap'),
+      maxOpacity: 0.4,
+      minOpacity: 0.6,
+      radius: 10,
+    });
+    /*this.heatmap.setData({
+      data: [
+        {x: 50, y: 50, value: 1}, // Living Room
+        {x: 90, y: 210, value: 1}, // Bedroom
+       ]
+    });*/
   }
 
   ngOnDestroy() {
--- src/providers/mqtt-client/mqtt-client.ts
+++ src/providers/mqtt-client/mqtt-client.ts
@@ -7,22 +7,43 @@ declare var Paho: any;
 export class MqttClientProvider {
   status: string = 'Disconnected';
   client: any = null;
-  message: any = '';
-  messageToSend: string = 'Your message';
+  //message: any = '';
+  //messageToSend: string = 'Your message';
   topic: string = 'swen325/a3';
   clientId: string = 'bhikhupras';
 
-  startDate: moment.Moment = moment().subtract(1,'days');
-  locationBuffer: any = {};
-  lastSeenLocations: any[]; // The message that holds the latest (parsed) message that had motion detected.
+  /**
+   * Holds latest sensor information on each location/room.
+   */
+  locationBuffer: any;
+
+    /**
+   * Holds a single or multiple sensor's information where motion was last detected.
+   */
+  locationBuffer_lastSeen: any[];
+
+  /**
+   * When there has not been any motion detection, which is common when application has just 
+   * started, the application will use this time to determine how long the individual has
+   * not been detected for.
+   */
+  noMotionTimer: moment.Moment = moment();
+
+  /**
+   * Data to help visualise motion on a Heatmap.
+   */
+  motionActivity_heatmapData: any[];
+  
+  /**
+   * Data to help count motion per location/room.
+   */
+  motionActivity_count: any;
 
   constructor() {
-
   }
 
   public connect = () => {
   	this.status = 'Connecting...';
-    //this.client = new Paho.MQTT.Client('m10.cloudmqtt.com', 31796, '/mqtt', this.clientId);
     let host = 'barretts.ecs.vuw.ac.nz';
     host = 'localhost';
   	this.client = new Paho.MQTT.Client(host, 8883, '/mqtt', this.clientId);
@@ -33,7 +54,6 @@ export class MqttClientProvider {
 
 	// connect the client
 	console.log('Connecting to mqtt via websocket');
-	//this.client.connect({timeout:10, userName:'ptweqash', password:'ncU6vlGPp1mN', useSSL:true, onSuccess:this.onConnect, onFailure:this.onFailure});
 	this.client.connect({timeout:10, useSSL:false, onSuccess:this.onConnect, onFailure:this.onFailure});
   }
 
@@ -41,16 +61,15 @@ export class MqttClientProvider {
   	if(this.status == 'Connected') {
   		this.status = 'Disconnecting...';
       this.client.disconnect();
-      //this.locationBuffer = {};
   		this.status = 'Disconnected';
   	}
   }
 
-  public sendMessage () {
+  /*public sendMessage () {
   	if(this.status == 'Connected') {
   		this.client.publish(this.topic, this.messageToSend);
   	}
-  }
+  }*/
 
   public onConnect = () => {
     console.log('Connecting/Connected - Subscribing');
@@ -61,13 +80,14 @@ export class MqttClientProvider {
       console.log('Connected - Subscribe Suceeded');
       this.status = 'Connected';
 
-      // Check if first time app has ran.
-      if(!((Object.keys(this.locationBuffer).length === 0 && this.locationBuffer.constructor === Object) && this.lastSeenLocations == undefined)) {
-        console.log('yeah')
-        this.startDate = moment();
+      // Checks if this is NOT the first time, that way we can reset the last seen timer.
+      if(this.locationBuffer && this.locationBuffer_lastSeen) {
+        this.noMotionTimer = moment();
       }
       this.locationBuffer = {};
-      this.lastSeenLocations = [];
+      this.locationBuffer_lastSeen = [];
+      this.motionActivity_heatmapData = [];
+      this.motionActivity_count = {};
     },
 		onFailure: (responseObject) => {
       console.log('Connected - Subscribe Failed');
@@ -89,14 +109,15 @@ export class MqttClientProvider {
 
   public onMessageArrived = (message) => {
   	//console.log('Received message: ', message.payloadString);
-    this.message = message.payloadString;
+    //this.message = message.payloadString;
     this.addMessageToBuffer(message.payloadString);
     
-    console.log(this.locationBuffer);
+    //console.log(this.locationBuffer);
+    //console.log(this.motionActivity_count)
   }
 
 
-  public addMessageToBuffer(message) {
+  addMessageToBuffer(message) {
     let messageSplit: string[] = message.split(',');
     
     let timestamp = moment(messageSplit[0]);
@@ -107,17 +128,32 @@ export class MqttClientProvider {
     this.locationBuffer = Object.assign(this.locationBuffer, { [location]: { timestamp, location, motion_status, battery_status }});
 
     if(motion_status === 1) {
-      for(let lastSeenLocation of this.lastSeenLocations) {
-        //console.log('lastSeenLocation: ', lastSeenLocation);
-        //console.log('timestamp:', timestamp);
-        //console.log('lastSeenLocation.timestamp: ', lastSeenLocation.timestamp);
-        //console.log('timestamp.isAfter(lastSeenLocation.timestamp): ', timestamp.isAfter(lastSeenLocation.timestamp));
+
+      let freshMotion = false;
+      for(let lastSeenLocation of this.locationBuffer_lastSeen) {
         if(timestamp.isAfter(lastSeenLocation.timestamp)) {
-          this.lastSeenLocations = [{ timestamp, location, motion_status, battery_status }];
-          return;
+          freshMotion = true;
+          break;
         }
       }
-      this.lastSeenLocations.push({ timestamp, location, motion_status, battery_status });
+
+      if(freshMotion) {
+        this.locationBuffer_lastSeen = [{ timestamp, location, motion_status, battery_status }];
+        this.noMotionTimer = timestamp;
+      } else {
+        this.locationBuffer_lastSeen.push({ timestamp, location, motion_status, battery_status });
+      }
+      let generateFunc = this.roomPos[location].bind(this);
+      this.motionActivity_heatmapData.push(generateFunc());
+      this.updateMotionActivityCount(location);
+    }
+  }
+
+  updateMotionActivityCount(location: string) {
+    if(this.motionActivity_count.hasOwnProperty(location)) {
+      this.motionActivity_count[location] = this.motionActivity_count[location] + 1;
+    } else {
+      this.motionActivity_count[location] = 1;
     }
   }
 
@@ -134,5 +170,44 @@ export class MqttClientProvider {
     } else {
       return str;
     }
+  }
+
+  roomPos: any = {
+    'bedroom' : this.generateBedroomPos,
+    'toilet' : this.generateToiletPos,
+    'living' : this.generateLivingRoomPos,
+    'dining' : this.generateDiningRoomPos,
+    'kitchen' : this.generateKitchenPos,
+  };
+
+  /*generateHousePos(): any {
+    let generateFunctions = [this.generateKitchenPos, this.generateBedroomPos, this.generateToiletPos, this.generateLivingRoomPos, this.generateDiningRoomPos];
+    let rnd_idx = this.getRandomInt(0, generateFunctions.length-1);
+    let func = generateFunctions[rnd_idx].bind(this);
+    return func();
+  }*/
+
+  generateBedroomPos(): any {
+    return {x: this.getRandomInt(15,105), y: this.getRandomInt(145,200)};
+  }
+
+  generateToiletPos(): any {
+    return {x: this.getRandomInt(135,180), y: this.getRandomInt(170,195)};
+  }
+
+  generateLivingRoomPos(): any {
+    return {x: this.getRandomInt(15,120), y: this.getRandomInt(15,115)};
+  }
+
+  generateDiningRoomPos(): any {
+    return {x: this.getRandomInt(250,350), y: this.getRandomInt(50,145)};
+  }
+
+  generateKitchenPos(): any {
+    return {x: this.getRandomInt(230,335), y: this.getRandomInt(178,180)};
+  }
+
+  getRandomInt(min, max): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
